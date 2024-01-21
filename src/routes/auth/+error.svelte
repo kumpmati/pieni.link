0c3f606a68dc509cc29179fbee69984dc5a9daa2
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-01-21 14:12:25 +0200

    add styling to auth pages

---
+++ src/routes/auth/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight">
+	<small class="text-muted-foreground">{$page.status}</small>
+	{$page.error?.message}
+</h1>
+
+<a href="/" class="mt-4">Back home</a>
