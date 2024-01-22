a6d23061ed30713b1e2253fc481447aa71b39aed
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-01-22 18:09:29 +0200

    remove commented out lines from hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,11 +5,5 @@ export const handle: Handle = async ({ event, resolve }) => {
 	// we can pass `event` because we used the SvelteKit middleware
 	event.locals.auth = auth.handleRequest(event);
 
-	// const session = await event.locals.auth.validate();
-
-	// if (!session && !event.url.pathname.startsWith('/auth')) {
-	// 	redirect(302, '/auth/signin');
-	// }
-
 	return await resolve(event);
 };
