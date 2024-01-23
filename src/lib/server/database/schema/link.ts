6ba7979a2959291e80f5d7de9b5d3add86b847b1
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-01-23 13:23:42 +0200

    add indexes to most used db columns

--- src/lib/server/database/schema/link.ts
+++ src/lib/server/database/schema/link.ts
@@ -1,18 +1,26 @@
-import { pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { index, pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { nanoid } from 'nanoid';
 import { z } from 'zod';
 import { user } from './auth';
 
-export const links = pgTable('links', {
-	id: text('id').primaryKey(),
-	url: text('url').notNull(),
-	createdAt: timestamp('created_at').notNull().defaultNow(),
-	lastUsed: timestamp('last_used'),
-	userId: varchar('user_id', { length: 15 })
-		.notNull()
-		.references(() => user.id, { onDelete: 'cascade', onUpdate: 'cascade' })
-});
+export const links = pgTable(
+	'links',
+	{
+		id: text('id').primaryKey(),
+		url: text('url').notNull(),
+		createdAt: timestamp('created_at').notNull().defaultNow(),
+		lastUsed: timestamp('last_used'),
+		userId: varchar('user_id', { length: 15 })
+			.notNull()
+			.references(() => user.id, { onDelete: 'cascade', onUpdate: 'cascade' })
+	},
+	(self) => ({
+		createdAtIdx: index('links_created_at_index').on(self.createdAt).desc(),
+		lastUsedIdx: index('links_last_used_index').on(self.lastUsed).desc(),
+		userIdIdx: index('links_user_id_index').on(self.userId)
+	})
+);
 
 export type Link = typeof links.$inferSelect;
 
