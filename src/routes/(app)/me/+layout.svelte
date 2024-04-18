111d0474a4b0e9182af311d6b9110dac88f6bd24
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-04-18 23:14:50 +0300

    remove unused code from layout

--- src/routes/(app)/me/+layout.svelte
+++ src/routes/(app)/me/+layout.svelte
@@ -1,11 +1,3 @@
-<script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import { IconLogout } from '@tabler/icons-svelte';
-	import type { LayoutData } from './$types';
-
-	export let data: LayoutData;
-</script>
-
 <div class="mt-12 flex flex-col gap-2">
 	<slot />
 </div>
