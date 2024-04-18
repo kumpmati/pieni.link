c6c1b654b8060c10e1b036b2329f5e2977b5b37a
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-04-19 01:41:17 +0300

    add temporary history of shortened links to front page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import { PUBLIC_BASEURL } from '$env/static/public';
-	import Button from '$lib/components/ui/button/button.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
+	import IconX from '~icons/tabler/x';
+	import IconCopy from '~icons/tabler/copy';
 	import type { Link } from '$lib/server/database/schema/link';
 	import { flyAndScale } from '$lib/utils';
 	import type { SubmitFunction } from '@sveltejs/kit';
-	import IconRotate from '~icons/tabler/rotate';
 	import { tick } from 'svelte';
 	import { toast } from 'svelte-sonner';
+	import * as Card from '$lib/components/ui/card';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { flip } from 'svelte/animate';
+	import { fly } from 'svelte/transition';
 
 	let form: HTMLFormElement;
 
+	let inputElement: HTMLInputElement;
 	let loading = false;
 	let shortenedLink: Link | null = null;
 	let error: any = null;
+	let links: Link[] = [];
 
-	const reset = () => {
-		loading = false;
-		shortenedLink = null;
-		error = null;
-	};
+	const copyLink = async (link: Link) => {
+		const url = `${PUBLIC_BASEURL}/${link.id}`;
 
-	const copyCurrentLink = async () => {
-		if (!shortenedLink) return;
-		const link = `${PUBLIC_BASEURL}/${shortenedLink.id}`;
-		await navigator.clipboard.writeText(link);
+		await navigator.clipboard.writeText(url);
+
 		toast.success('Link copied!', {
-			description: `Copied '${link}' to clipboard.`,
+			description: `Copied '${url}' to clipboard.`,
 			dismissable: true
 		});
 	};
@@ -37,7 +38,7 @@
 		form.requestSubmit();
 	};
 
-	const handleSubmit: SubmitFunction = () => {
+	const handleSubmit: SubmitFunction = ({ formElement }) => {
 		loading = true;
 		shortenedLink = null;
 		error = null;
@@ -46,7 +47,8 @@
 			switch (result.type) {
 				case 'success':
 					shortenedLink = result.data as Link;
-					await copyCurrentLink();
+					links = [shortenedLink, ...links];
+					await copyLink(shortenedLink);
 					break;
 
 				case 'error':
@@ -76,39 +78,55 @@
 		action="?/create"
 		class="flex w-full items-center gap-0"
 	>
-		{#if !shortenedLink}
-			<Input
-				autofocus
-				name="url"
-				type="url"
-				required
-				placeholder="Paste a link here to shorten it"
-				on:paste={handleOnPaste}
-				disabled={loading}
-				class="text-md bg-slate-800 p-5 md:px-5 md:py-6 md:text-lg"
-			/>
-		{:else}
-			<Input
-				name="url"
-				type="url"
-				readonly
-				on:focus={(e) => e.currentTarget.select()}
-				value="{PUBLIC_BASEURL}/{shortenedLink.id}"
-				class="text-md rounded-r-none bg-slate-800 p-5 md:px-5 md:py-6 md:text-lg"
-			/>
-
-			<Button
-				type="reset"
-				on:click={reset}
-				size="icon"
-				variant="secondary"
-				title="Shorten another link"
-				class="h-full w-fit rounded-l-none border-l-0 px-4 py-2"
-			>
-				<IconRotate width={20} height={20} />
-			</Button>
-		{/if}
+		<!-- svelte-ignore a11y-autofocus -->
+		<input
+			autofocus
+			bind:this={inputElement}
+			name="url"
+			type="url"
+			required
+			placeholder="Paste a link here to shorten it"
+			on:paste={handleOnPaste}
+			disabled={loading}
+			class="text-md flex h-9 w-full rounded-md border border-input bg-slate-800 p-5 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:px-5 md:py-6 md:text-lg"
+			value={shortenedLink ? `${PUBLIC_BASEURL}/${shortenedLink.id}` : ''}
+		/>
 	</form>
+
+	{#if links.length > 0}
+		<ul class="mt-2 flex w-full flex-col gap-2" in:fly={{ duration: 200, y: -5 }}>
+			{#each links as link, i (link.id)}
+				<li animate:flip={{ duration: 200 }}>
+					<Card.Root class="relative flex w-full px-4 py-3">
+						<Card.Header class="p-0">
+							<Card.Title class="inline-flex gap-1 text-sm">
+								{`${PUBLIC_BASEURL}/${link.id}`}
+
+								<Button variant="ghost" size="icon" class="h-6 w-6" on:click={() => copyLink(link)}>
+									<IconCopy width={12} height={12} />
+								</Button>
+							</Card.Title>
+							<Card.Description class="text-xs">
+								{link.url}
+							</Card.Description>
+						</Card.Header>
+
+						<Button
+							variant="ghost"
+							size="icon"
+							class="absolute right-2 top-2 ml-auto"
+							on:click={() => {
+								links.splice(i, 1);
+								links = links;
+							}}
+						>
+							<IconX width={16} height={16} />
+						</Button>
+					</Card.Root>
+				</li>
+			{/each}
+		</ul>
+	{/if}
 
 	{#if error}
 		<p class="text-destructive" transition:flyAndScale>
