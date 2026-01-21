5cf9ef17373b7508959d4a810307113e1b471eb7
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2026-01-21 19:18:43 +0200

    fix: add loading state to own links

--- src/routes/(app)/OwnLinksTable.svelte
+++ src/routes/(app)/OwnLinksTable.svelte
@@ -17,44 +17,72 @@
 <table>
 	<thead>
 		<tr>
-			<td width="100">Slug</td>
+			<td width="120">Slug</td>
 			<td>URL</td>
-			<td>Last visit</td>
+			<td width="140" class="desktop-only">Last visit</td>
 			<td width="32"></td>
 		</tr>
 	</thead>
 
 	<tbody>
-		{#each await getAllOwnLinks(showAll ? 1_000 : 5) as link}
-			<tr class="row">
-				<td>
-					<a href="/l/{link.id}">{link.id}</a>
-				</td>
-				<td>
-					<a class="external-link" href={link.url} target="_blank" rel="noopener noreferrer">
-						{new URL(link.url).host}
+		<svelte:boundary>
+			{#snippet pending()}
+				{#each { length: 3 }}
+					<tr class="row">
+						<td>...</td>
+						<td>...</td>
+						<td class="desktop-only">...</td>
+						<td>
+							<div class="button-placeholder"></div>
+						</td>
+					</tr>
+				{/each}
+			{/snippet}
 
-						<span class="icon">
-							<LucideExternalLink width={16} height={16} />
-						</span>
-					</a>
-				</td>
-				<td>
-					<p class="subtle" title={link.lastUsed?.toLocaleString() ?? '-'}>
-						{link.lastUsed ? dayjs(link.lastUsed).fromNow() : '-'}
-					</p>
-				</td>
-				<td>
-					<Button variant="text" iconType="full" href="/l/{link.id}">
-						<LucideArrowRight width={18} height={18} />
-					</Button>
-				</td>
-			</tr>
-		{/each}
+			{#each await getAllOwnLinks(showAll ? 1_000 : 5) as link}
+				<tr class="row">
+					<td>
+						<a href="/l/{link.id}">{link.id}</a>
+					</td>
+					<td>
+						<a class="external-link" href={link.url} target="_blank" rel="noopener noreferrer">
+							{new URL(link.url).host}
+
+							<span class="icon">
+								<LucideExternalLink width={16} height={16} />
+							</span>
+						</a>
+					</td>
+					<td class="desktop-only">
+						<p class="subtle" title={link.lastUsed?.toLocaleString() ?? '-'}>
+							{link.lastUsed ? dayjs(link.lastUsed).fromNow() : '-'}
+						</p>
+					</td>
+					<td>
+						<Button variant="text" iconType="full" href="/l/{link.id}">
+							<LucideArrowRight width={18} height={18} />
+						</Button>
+					</td>
+				</tr>
+			{:else}
+				<tr>
+					<td colspan="4">
+						<span class="subtle" style="text-align:center;display:block;margin:1rem 0;"
+							>No links yet.</span
+						>
+					</td>
+				</tr>
+			{/each}
+		</svelte:boundary>
 	</tbody>
 </table>
 
 <style>
+	a,
+	p {
+		margin: 0;
+	}
+
 	thead {
 		font-weight: 500;
 		color: var(--m3c-secondary);
@@ -75,6 +103,16 @@
 	.subtle {
 		font-size: 14px;
 		color: var(--m3c-on-surface-variant);
+	}
+
+	.button-placeholder {
+		height: 40px;
+	}
+
+	.desktop-only {
+		@media screen and (max-width: 600px) {
+			display: none;
+		}
 	}
 
 	.external-link {
